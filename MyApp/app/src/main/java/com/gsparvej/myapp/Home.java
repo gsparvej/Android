b6dafae3774864b2c184c09a30da425d61be1d26
gsparvej <parvejmd4469@gmail.com>
2025-09-03 12:13:40 +0600

    added

--- MyApp/app/src/main/java/com/gsparvej/myapp/Home.java
+++ MyApp/app/src/main/java/com/gsparvej/myapp/Home.java
@@ -1,6 +1,9 @@
 package com.gsparvej.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,83 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
 
+    private Button btnScienceFic , btnBengaliHis , btnMaths , btnPhy , btnHealth , btnTech;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnScienceFic = findViewById(R.id.btnScienceFik);
+        btnBengaliHis = findViewById(R.id.btnBengali);
+        btnMaths = findViewById(R.id.btnMaths);
+        btnPhy = findViewById(R.id.btnPhy);
+
+        btnScienceFic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ScienceFiction.class);
+                startActivity(intent);
+            }
         });
+
+
+
+        btnBengaliHis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BegaliHistory.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        btnMaths.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Maths.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        btnPhy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Physics.class);
+                startActivity(intent);
+            }
+        });
+
+        btnHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Health.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        btnTech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Technology.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
     }
+
+
 }
